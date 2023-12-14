16b9921da82a811054ae789b9080255f0fb251a5
Stes315770 <stes315770@gmail.com>
2023-12-14 09:37:39 -0700

    Imported classes, started work on savings account functions

--- customer_banking.py
+++ customer_banking.py
@@ -1,5 +1,7 @@
 # Import the create_cd_account and create_savings_account functions
 # ADD YOUR CODE HERE
+from savings_account import create_savings_account
+from cd_account import create_cd_account
 
 # Define the main function
 def main():
@@ -8,7 +10,9 @@ def main():
     It displays the interest earned on the savings and CD accounts and updates the balances.
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
-    # ADD YOUR CODE HERE
+    savings_balance = input("Set initial savings balance: ")
+    savings_interest = input("Set interest rate: ")
+    savings_maturity = input("Set number of months for savings to mature: ")
 
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
@@ -17,7 +21,9 @@ def main():
     # ADD YOUR CODE HERE
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
-    # ADD YOUR CODE HERE
+    cd_balance = 
+    cd_interest = 
+    cd_maturity = 
 
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
@@ -27,3 +33,4 @@ def main():
 
 if __name__ == "__main__":
     # Call the main function.
+    main()
